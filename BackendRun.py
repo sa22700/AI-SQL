b57e18d0718a966fcb11ff9bff5f8daf59bf1510
sa22700 <pirkka.toivakka@gmail.com>
2026-02-25 16:31:07 +0200

    backend start

---
+++ BackendRun.py
@@ -0,0 +1,19 @@
+import os
+import uvicorn
+
+def main():
+    app = os.environ['UVICORN_APP']
+    host = os.environ['UVICORN_HOST']
+    port = int(os.environ['UVICORN_PORT'])
+    reload_ = os.environ['UVICORN_RELOAD']
+
+    uvicorn.run(
+        app,
+        host=host,
+        port=port,
+        reload=reload_,
+        log_level=os.environ['UVICORN_LOG_LEVEL'],
+    )
+
+if __name__ == "__main__":
+    main()
